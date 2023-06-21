7f05b2ece19b204b5e533f3f5b852d1956e5e2bb
SaidNivek <kevin.vi.dias@gmail.com>
2023-06-20 23:10:09 -0400

    First render of hello world

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  console.log("Test")
+  const message = "Hello"
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: 100, width: 100, backgroundColor: 'orange', display: 'flex', justifyContent: 'center'}}>
+        <Text>{message} World</Text>
     </View>
   );
 }
