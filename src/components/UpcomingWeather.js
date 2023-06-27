7edaecd6aff3e9fb2bf4089109345b416096c049
SaidNivek <kevin.vi.dias@gmail.com>
2023-06-26 23:25:51 -0400

    Begin coding FlatList for UpcomingWeather.js

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, FlatList } from 'react-native';
 
 const UpcomingWeather = () => {
+    const DATA = [
+        {
+            dt_txt: '2023-02-18 12:00:00',
+            main: {
+                temp_max: 8.55,
+                temp_min: 7.55
+            },
+            weather: [
+                {
+                    main: 'Clear'
+                }
+            ]
+        },
+        {
+            dt_txt: '2023-02-18 15:00:00',
+            main: {
+                temp_max: 8.55,
+                temp_min: 7.55
+            },
+            weather: [
+                {
+                    main: 'Rain'
+                }
+            ]
+        },
+        {
+            dt_txt: '2023-02-18 18:00:00',
+            main: {
+                temp_max: 8.55,
+                temp_min: 7.55
+            },
+            weather: [
+                {
+                    main: 'Windy'
+                }
+            ]
+        }
+    ]
+
     return (
         <SafeAreaView styles={styles.container}>
             <Text>Upcoming Weather</Text>
+            <FlatList 
+                data={DATA}
+                renderItem={}
+            />
         </SafeAreaView>
     )
 }
