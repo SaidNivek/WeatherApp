85a53c045e11f2c0add0d3d6702c5dcbf25fbd7b
SaidNivek <kevin.vi.dias@gmail.com>
2023-06-27 23:38:36 -0400

    Styles added to upcoming weather page

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,85 +1,111 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, FlatList, View } from 'react-native';
+import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  View,
+  StatusBar
+} from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 const DATA = [
-    {
-        dt_txt: '2023-02-18 12:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55
-        },
-        weather: [
-            {
-                main: 'Clear'
-            }
-        ]
+  {
+    dt_txt: '2023-02-18 12:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
     },
-    {
-        dt_txt: '2023-02-18 15:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55
-        },
-        weather: [
-            {
-                main: 'Rain'
-            }
-        ]
+    weather: [
+      {
+        main: 'Clear'
+      }
+    ]
+  },
+  {
+    dt_txt: '2023-02-18 15:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
     },
-    {
-        dt_txt: '2023-02-18 18:00:00',
-        main: {
-            temp_max: 8.55,
-            temp_min: 7.55
-        },
-        weather: [
-            {
-                main: 'Windy'
-            }
-        ]
-    }
-]    
+    weather: [
+      {
+        main: 'Rain'
+      }
+    ]
+  },
+  {
+    dt_txt: '2023-02-18 18:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Windy'
+      }
+    ]
+  }
+]
 
 const Item = (props) => {
-    const { dt_txt, min, max, condition } = props
-    return (
-        <View>
-            <Feather name={'sun'} size={50} color={'white'} />
-            <Text>{dt_txt}</Text>
-            <Text>{min}</Text>
-            <Text>{max}</Text>
-        </View>
-    )
+  const { dt_txt, min, max, condition } = props
+  return (
+    <View style={styles.item}>
+      <Feather name={'sun'} size={50} color={'white'} />
+      <Text style={styles.date}>{dt_txt}</Text>
+      <Text style={styles.temp}>{min}</Text>
+      <Text style={styles.temp}>{max}</Text>
+    </View>
+  )
 }
 
 const UpcomingWeather = () => {
-    const renderItem = ({item}) => (
-        <Item 
-            condition={item.weather[0].main} 
-            dt_txt={item.dt_txt}    
-            min={item.main.temp_min}    
-            max={item.main.temp_max}                
-        />
-    )
-   
+  const renderItem = ({ item }) => (
+    <Item
+      condition={item.weather[0].main}
+      dt_txt={item.dt_txt}
+      min={item.main.temp_min}
+      max={item.main.temp_max}
+    />
+  )
 
-    return (
-        <SafeAreaView styles={styles.container}>
-            <Text>Upcoming Weather</Text>
-            <FlatList 
-                data={DATA}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.dt_txt}
-            />
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Upcoming Weather</Text>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.dt_txt}
+      />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'aqua' // a constant taking in the current height of the status bar or use 0
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderWidth: 5,
+    backgroundColor: 'teal'
+  },
+  temp: {
+    color: 'white',
+    fontSize: 20
+  },
+  date: {
+    color: 'white',
+    fontSize: 15
+  }
 })
 
 export default UpcomingWeather
