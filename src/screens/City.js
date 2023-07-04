9fce823f0c5fdc15c988157ae412ea14bf50ef99
SaidNivek <kevin.vi.dias@gmail.com>
2023-07-03 22:04:56 -0400

    Connect basic City screen to check it's connected

--- src/screens/City.js
+++ src/screens/City.js
@@ -3,8 +3,16 @@ import { SafeAreaView, Text, StyleSheet } from 'react-native'
 
 const City = () => {
     return (
-        
+        <SafeAreaView style={styles.container}>
+            <Text>City</Text>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
 
 export default City
