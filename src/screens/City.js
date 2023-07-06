5c8004d2de2617c06209824bd2982ec740207269
SaidNivek <kevin.vi.dias@gmail.com>
2023-07-05 22:46:40 -0400

    Hard code city and country with styles

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,18 +1,48 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  StatusBar
+} from 'react-native'
 
 const City = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>City</Text>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/city-bkg.jpg')}
+        style={styles.imageLayout}
+      >
+        <Text style={[styles.cityName]}>London</Text>
+        <Text style={[styles.countryName]}>United Kingdom</Text>
+      </ImageBackground>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0
+  },
+  imageLayout: {
+    flex: 1
+  },
+  cityName: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  countryName: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold'
+  }
 })
 
 export default City
