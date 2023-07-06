f23096ae438c9c1d7cfed0b70393adffe9c9fc79
SaidNivek <kevin.vi.dias@gmail.com>
2023-07-05 22:52:39 -0400

    Add population hard-coded with styles to City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -4,8 +4,10 @@ import {
   Text,
   StyleSheet,
   ImageBackground,
-  StatusBar
+  StatusBar,
+  View
 } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const City = () => {
   return (
@@ -16,6 +18,10 @@ const City = () => {
       >
         <Text style={[styles.cityName, styles.cityText]}>London</Text>
         <Text style={[styles.countryName, styles.cityText]}>UK</Text>
+        <View style={styles.populationWrapper}>
+          <Feather name="user" size={50} color="black" />
+          <Text style={styles.populationText}>8000</Text>
+        </View>
       </ImageBackground>
     </SafeAreaView>
   )
@@ -40,6 +46,17 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     fontWeight: 'bold',
     color: 'white'
+  },
+  populationWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30
+  },
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    fontWeight: 'bold'
   }
 })
 
