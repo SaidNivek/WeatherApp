4ae20ae1c6d816da2a7ea1d16648d60bab09146d
SaidNivek <kevin.vi.dias@gmail.com>
2023-07-10 23:22:49 -0400

    Begin refactoring city component

---
+++ src/components/IconText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const IconText = (props) => {
+  return <div>IconText</div>
+}
+
+export default IconText
