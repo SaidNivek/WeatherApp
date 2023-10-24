d7c6a5d1e2a4d2a890a1628df156f1ace0191a27
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-24 00:26:59 -0400

    Add Location data from phone and access to that information

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('Permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -12,6 +32,7 @@ const App = () => {
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
