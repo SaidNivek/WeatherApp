ee6c6b829fa85de47c224db4c927608b4af44c51
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-24 00:39:24 -0400

    Setup the .env file and add react-native-dotenv file to babel.config.js

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 import * as Location from 'expo-location'
+import { TEST_KEY } from '@env'
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+  console.log(TEST_KEY)
 
   useEffect(() => {
-    (async () => {
+    ;(async () => {
       let { status } = await Location.requestForegroundPermissionsAsync()
       if (status !== 'granted') {
         setError('Permission to access location was denied')
