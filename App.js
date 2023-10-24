ae053474c196d7fc6a3f87e9ebf872216a735a50
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-24 00:09:45 -0400

    Add spinner if loading is true

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'teal'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
@@ -10,4 +19,10 @@ const App = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 export default App
