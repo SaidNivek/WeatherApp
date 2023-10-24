65094f7c25b4c248c631f8b0ff5925e61704685d
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-24 10:11:57 -0400

    Add OurButton comp to play with buttons in react native

--- App.js
+++ App.js
@@ -2,16 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { ActivityIndicator, View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
-import { WEATHER_API_KEY } from '@env'
 import { useGetWeather } from './src/hooks/useGetWeather'
 import ErrorItem from './src/components/ErrorItem'
 
 const App = () => {
   const [loading, error, weather] = useGetWeather()
-  // console.log('fetch')
-  // if (weather) {
-  //   console.log(weather)
-  // }
 
   if (weather && weather.list && !loading) {
     return (
